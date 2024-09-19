1b6cca4ea64832e1a835a0adfbc7734e51c7392b
iloveicecream0829 <queeniez7@nycstudents.net>
2024-09-19 09:20:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-write_a_sentence = input("write a sentence ")
-if write_a_sentence == "Do you like icecream? "
-    print 
+write_a_sentence = input("write a sentence")
+if write_a_sentence == "Do you like icecream?":
+    print("correct")
+else:
+    print("incorrect")
