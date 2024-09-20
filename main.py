9b6eb7dbc6ea5ea36470f019a15f18b72207b4c1
iloveicecream0829 <queeniez7@nycstudents.net>
2024-09-20 09:19:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ if write_a_sentence == "Do you like icecream?":
     print("correct")
 else:
     print("incorrect")
+
+
+def evenodd():
+evenodd = int(input ("input a number: "))
+if evenodd % 2 == 0:
+    print("even")
+else:
+    print("odd")
