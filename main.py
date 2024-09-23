fdc555615d858608a6d2fe3001c201877c47bd48
iloveicecream0829 <queeniez7@nycstudents.net>
2024-09-23 09:20:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-write_a_sentence = input("write a sentence")
-if write_a_sentence == "Do you like icecream?":
-    print("correct")
-else:
-    print("incorrect")
+# write_a_sentence = input("write a sentence")
+# if write_a_sentence == "Do you like icecream?":
+#     print("correct")
+# else:
+#     print("incorrect")
 
 
 def evenodd():
-evenodd = int(input ("input a number: "))
-if evenodd % 2 == 0:
-    print("even")
-else:
-    print("odd")
+    evenodd = int(input("input a number: "))
+    if evenodd % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+evenodd()
+
+
+bill = int(input("Whats the bill?"))
+
+def thing(bill):
+        print(int(bill))
