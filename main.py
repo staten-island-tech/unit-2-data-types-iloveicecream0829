27705b9afe78690fb8828c87efbee1f5feff561f
iloveicecream0829 <queeniez7@nycstudents.net>
2024-09-30 09:21:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -31,11 +31,31 @@
 
 # thing()
 
-def gcf(a, b):
+def fac():
+        num = int(input("Please enter the number you want the factor of."))
+        
+
+
+
+
+
+
+
+
+
+
+
+
+def fgcf(a, b):
+        while b:
+                a, b = b, a % b
+        return a
+
+def gcf():
         a = int(input("Input a number you want the gcf of: "))
         b = int(input("Input another number you want the gcf of: "))
-        gcf = fgcf(a, b)
-        print(f"The factor of {a} and {b} is {gcf}.")
+        gcf_value = fgcf(a, b)
+        print(f"The factor of {a} and {b} is {gcf_value}.")
 
-gcf(a, b)
+gcf()
         
